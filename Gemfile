99fc4576badd877511183910e546bdfa683a9d53
Don Dumont <imdondumont@hotmail.com>
2023-03-01 21:24:53 -0800

    update ruby, jekyll, SNS29 post

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ gem 'jekyll-sitemap'
 gem 'jekyll-paginate-v2'
 gem 'kramdown-parser-gfm'
 gem "alembic-jekyll-theme"
+gem "webrick", "~> 1.8"
--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20221128215919';
+const version = '20230301212344';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/swig-n'-sealed/2022/11/28/SNS28.html","/swig-n'-sealed/2022/09/28/SNS27.html","/drafts-and-draft/2022/06/13/DND-CLB.html","/swig-n'-sealed/2022/05/02/SNS26.html","/news/2022/02/22/state-of-disorganized-play-1.html","/swig-n'-sealed/2022/02/13/SNS25.html","/news/2021/11/09/Were-Back-Update.html","/swig-n'-sealed/2021/11/09/SNS-24.html","/news/2020/06/12/Covid-Update.html","/news/2020/03/22/Virtual-FNMs.html","/about/","/calendar/","/discord/","/feed.xml","/css/main.css","/patreon/","/posts/","/assets/styles.css","/subscribe/","/manifest.json","/assets/search.json","/redirects.json","/sitemap.xml","/robots.txt","/index.html","/page/2/index.html","/page/3/index.html","/css/main.css.map","/assets/styles.css.map","/assets/logo.svg", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/swig-n'-sealed/2023/03/01/SNS29.html","/swig-n'-sealed/2022/11/28/SNS28.html","/swig-n'-sealed/2022/09/28/SNS27.html","/drafts-and-draft/2022/06/13/DND-CLB.html","/swig-n'-sealed/2022/05/02/SNS26.html","/news/2022/02/22/state-of-disorganized-play-1.html","/swig-n'-sealed/2022/02/13/SNS25.html","/news/2021/11/09/Were-Back-Update.html","/swig-n'-sealed/2021/11/09/SNS-24.html","/news/2020/06/12/Covid-Update.html","/about/","/calendar/","/discord/","/feed.xml","/css/main.css","/patreon/","/posts/","/assets/styles.css","/subscribe/","/manifest.json","/assets/search.json","/redirects.json","/sitemap.xml","/robots.txt","/index.html","/page/2/index.html","/page/3/index.html","/page/4/index.html","/css/main.css.map","/assets/styles.css.map","/assets/logo.svg", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
